e343a453c30d7175fd7fe986c316ac0795143c68
Alexander <melnikov.alexander21@gmail.com>
2020-04-12 15:14:16 +0300

    Added 2 methods: read and write to file

--- 4 laba OOP 4 sem/FileManager.h
+++ 4 laba OOP 4 sem/FileManager.h
@@ -8,18 +8,45 @@ template<class T>
 class FileManager
 {
 public:
-	static void readFile(string fileName);
-	static void writeFile(string fileName);
+	static int readFile(string fileName, vector<T>& vec);
+	static int writeFile(string fileName, vector<T>& vec);
 };
 
 template<class T>
-inline void FileManager<T>::readFile(string fileName)
+inline int FileManager<T>::readFile(string fileName, vector<T>& vec)
 {
+	ifstream fin(fileName);
+	if (!fin.is_open()) 
+	{
+		ofstream fout(fileName);
+		if (!fout) { return 0; }
+		fout.close();
+		return 1;
+	}
 
+	if (!fin) { return 0; }
+	while (true)
+	{
+		T obj;
+		fin >> obj;
+		if (fin.eof()) { break; }
+		if (!fin) { return 0; }
+		vec.push_back(obj);
+	}
+	fin.close();
+	return 1;
 }
 
 template<class T>
-inline void FileManager<T>::writeFile(string fileName)
+inline int FileManager<T>::writeFile(string fileName, vector<T>& vec)
 {
-
+	ofstream fout(fileName, ios_base::trunc);
+	if (!fout) { return 0; }
+	for (size_t i = 0; i < vec.size(); i++)
+	{
+		fout << vec[i];
+		if (!fout) { return 0; }
+	}
+	fout.close();
+	return 1;
 }
